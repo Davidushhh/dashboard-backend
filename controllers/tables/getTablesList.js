4328c729061e9dfa39ed196d5607b15bb32e3950
Nick Leso <nickleso.work@gmail.com>
2023-12-25 14:17:40 +0200

    Update getTablesList.js
    - add error handling and universal tables render

--- controllers/tables/getTablesList.js
+++ controllers/tables/getTablesList.js
@@ -4,8 +4,7 @@ const getTablesList = async (req, res, next) => {
   const { id } = req.user;
   const { DB } = process.env;
 
-  const tablesAccessQuery = `SELECT tablesAccess 
-    FROM dep_users WHERE id = ?;`;
+  const tablesAccessQuery = `SELECT tablesAccess FROM dep_users WHERE id = ?;`;
 
   try {
     pool.query(tablesAccessQuery, [id], async (err, result) => {
@@ -30,46 +29,51 @@ const getTablesList = async (req, res, next) => {
           const updateTimeQuery = `
             SELECT UPDATE_TIME, vs.serviceName_cyrillic, vs.documents, vs.details
             FROM information_schema.tables t
-            LEFT JOIN veteranServices_list vs ON t.table_name = vs.tableName
+            LEFT JOIN tablesMeta_list vs ON t.table_name = vs.tableName
             WHERE t.table_schema = ?
             AND t.table_name = ?;
           `;
 
           pool.query(updateTimeQuery, [DB, tableName], (err, result) => {
-            console.log(result);
-
             if (err) {
               reject(err);
             } else {
-              if (!result || result.length === 0) {
-                return res.status(404).json({
+              if (!result || result.length === 0 || result === undefined) {
+                reject({
+                  status: 404,
                   message: "not found data",
                   code: 404,
                 });
+              } else {
+                resolve({
+                  tableName,
+                  serviceName_cyrillic: result[0].serviceName_cyrillic,
+                  documents: result[0].documents?.split(","),
+                  details: result[0].details,
+                  updateTime: result[0].UPDATE_TIME,
+                });
               }
-
-              resolve({
-                tableName,
-                serviceName_cyrillic: result[0].serviceName_cyrillic,
-                documents: result[0].documents.split(","),
-                details: result[0].details,
-                updateTime: result[0].UPDATE_TIME,
-              });
             }
           });
         });
       });
 
-      const tablesInfo = await Promise.all(tablesInfoPromises);
-
-      return res.status(200).json({
-        message: "tables list with update time and additional data",
-        code: 200,
-        data: tablesInfo,
-      });
+      Promise.all(tablesInfoPromises)
+        .then((tablesInfo) => {
+          return res.status(200).json({
+            message: "tables list with update time and additional data",
+            code: 200,
+            data: tablesInfo,
+          });
+        })
+        .catch((error) => {
+          return res.status(500).json({
+            message: error.message,
+            code: 500,
+          });
+        });
     });
   } catch (error) {
-    console.log(error);
     return res.status(500).json({
       message: error.message,
       code: 500,
