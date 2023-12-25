c53e0aa854ae2e9462f72c5690722dd20417d204
Nick Leso <nickleso.work@gmail.com>
2023-12-25 16:51:26 +0200

    Update app.js
    - json limit increase

--- app.js
+++ app.js
@@ -21,8 +21,8 @@ const accessLogStream = fs.createWriteStream(
 
 app.use(logger(formatsLogger, { stream: accessLogStream }));
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ limit: "30mb" }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
 
 app.use("/api/charts", chartsRouter);
 app.use("/api/auth", authRouter);
