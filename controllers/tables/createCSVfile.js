1dee107c973a41e268549836726eb8d3d8eaf23f
Nick Leso <nickleso.work@gmail.com>
2023-12-27 10:40:47 +0200

    Update createCSVfile.js

--- controllers/tables/createCSVfile.js
+++ controllers/tables/createCSVfile.js
@@ -1,5 +1,3 @@
-// const fs = require("fs");
-// const path = require("path");
 const { dateTransformer } = require("../../helpers/dateTransformer");
 
 const createCSVfile = async (req, res, next) => {
